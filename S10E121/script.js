33d37c7a39a902642ebeb2efff042489e101b7b5
Luiz <lg.motta@hotmail.com>
2020-08-13 21:50:32 -0300

    some excercises

---
+++ S10E121/script.js
@@ -0,0 +1,5 @@
+var firstName = prompt("Hey stranger, what is your first name?")
+var lastName = prompt("Good... Now what is your last name?")
+var age = prompt("Hey "+ firstName + " aren't you too young to walk here? By the way, how old are you?")
+alert("Let me see straight, " + firstName + " " + lastName + ", you are only " + age + "and already wondering alone in this place?")
+
